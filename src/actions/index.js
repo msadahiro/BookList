56664c3fdef980887480637878a53fb7249404d7
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-04-23 12:29:06 -0700

    able to click and show one book

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
 export function selectBook(book){
-    console.log('A book has been selected', book.title)
+    // selectBook is an ActionCreator, it needs to reutn an action
+    // an object with a type property
+    // actions have two values... type and sometimes.. a payload
+    // type is ALWAYS UPPERCASE
+    return {
+        type:'BOOK_SELECTED',
+        payload:book
+    };
 }
